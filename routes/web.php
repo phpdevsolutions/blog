d4e5a86d55bcc59cc3409fa8dbd248abb3014fc1
Marcos <tmlwar01@gmail.com>
2021-11-19 17:41:36 -0600

    Filtrado de posts por categorias

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::get('/', [PostController::class, 'index'])->name('post.index');
 
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
+Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
