8257554ed64665f25e838f70b728b55b76963ef4
Marcos <tmlwar01@gmail.com>
2021-11-19 12:04:55 -0600

    Mostrar detalle de un post

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
