58444c7a38ef2ade7347f04b53dd8687b30b8f91
Marcos <tmlwar01@gmail.com>
2021-12-22 19:59:02 -0600

    Listado de posts

--- config/adminlte.php
+++ config/adminlte.php
@@ -265,60 +265,19 @@ return [
             'icon' => 'far fa-fw fa-bookmark',
             'active' => ['admin/tags*']
         ],
+        
+        ['header' => 'Opciones de Blog'],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text'       => 'Lista de Post',
+            'route'        => 'admin.posts.index',
+            'icon'      => 'fas fa-fw fa-clipboard'
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
+            'text'       => 'Crear nuevo Post',
+            'route'        => 'admin.posts.create',
+            'icon'      => 'fas fa-fw fa-file'
         ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        
     ],
 
     /*
@@ -473,5 +432,5 @@ return [
     |
     */
 
-    'livewire' => false,
+    'livewire' => true,
 ];
---
+++ resources/views/livewire/admin/posts-index.blade.php
@@ -0,0 +1,48 @@
+<div class="card">
+
+    <div class="card-header">
+        <input wire:model="search" type="text" class="form-control" placeholder="Ingresa el nombre del Post">
+    </div>
+    
+    @if ($posts->count())
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->name}}</td>
+                            <td width="10px">
+                                <a href="{{route('admin.posts.edit', $post)}}" class="btn btn-primary btn-sm">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            {{$posts->links()}}
+        </div>
+    @else
+        <div class="card-body">
+            <strong>No se encontraron registros</strong>
+        </div>
+    @endif
+</div>
