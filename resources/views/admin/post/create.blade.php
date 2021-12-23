8a9e25cdfe01f1739196005776e8a916478e9d14
Marcos <tmlwar01@gmail.com>
2021-12-22 20:57:11 -0600

    Crear nuevo post

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -7,7 +7,94 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
+
+                {!! Form::hidden('user_id', auth()->user()->id) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre:') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del post']) !!}
+
+                    @error('name')
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('slug', 'SLug:') !!}
+                    {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el slug del post', 'readonly']) !!}
+
+                    @error('slug')
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('cateogry_id', 'Categoría') !!}
+                    {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+
+                    @error('category_id')
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <p class="font-wight-bold">Etiquetas</p>
+                    @foreach ($tags as $tag)
+                        <label class="mr-2">
+                            {!! Form::checkbox('tags[]', $tag->id, null, ) !!}
+                            {{$tag->name}}
+                        </label>
+                    @endforeach
+
+                    
+                    @error('tags')
+                        <br>
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <p class="font-wight-bold">Estado</p>
+                    <label>
+                        {!! Form::radio('status', 1, true) !!}
+                        Borrador
+                    </label>
+                    <label>
+                        {!! Form::radio('status', 2) !!}
+                        Publicado
+                    </label>
+
+                    @error('status')
+                        <br>
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('extract', 'Extracto') !!}
+                    {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+
+                    @error('extract')
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('body', 'Cuerpo del post') !!}
+                    {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+
+                    @error('body')
+                        <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
+                {!! Form::submit('Crear post', ['class' => 'btb btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 
 @section('css')
@@ -15,5 +102,28 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/31.1.0/classic/ckeditor.js"></script>
+
+    <script>
+        $(document).ready( function() {
+            $("#name").stringToSlug({
+              setEvents: 'keyup keydown blur',
+              getPut: '#slug',
+              space: '-'
+            });
+          });
+
+        ClassicEditor
+        .create( document.querySelector( '#extract' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+        ClassicEditor
+        .create( document.querySelector( '#body' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    </script>
+    
+@endsection
--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Administrador')
 
 @section('content_header')
+    <a class="btn btn-secondary btn-sm float-right" href="{{route('admin.posts.create')}}">Nuevo Post</a>
     <h1>Listado de Post</h1>
 @stop
 
